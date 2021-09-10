dfbf72af0084bdfef64c13651a436240212ea073
skkanna <64684118+skkanna@users.noreply.github.com>
2021-09-10 14:37:41 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 18:45:05 2020
+
+@author: KamalaKannan
+"""
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from keras.preprocessing import image
+
+class dogcat:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    def predictiondogcat(self):
+        # load model
+        model = tf.keras.models.load_model('model.h5')
+
+        # summarize model
+        #model.summary()
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = model.predict(test_image)
+
+        if result[0][0] == 1:
+            prediction = 'Present'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Not Present'
+            return [{ "image" : prediction}]
+
+
